37da313f362482ba5db9b49ff23bd95b10fba38a
ritesh <ritesh@gmail.com>
2025-08-08 15:53:10 +0530

    product dto

--- src/main/java/com/oms/Service/ProductService.java
+++ src/main/java/com/oms/Service/ProductService.java
@@ -2,10 +2,13 @@ package com.oms.Service;
 
 import com.oms.Repo.ProductRepo;
 import com.oms.dto.ProductRequest;
+import com.oms.dto.ProductResponse;
 import com.oms.entity.Product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +19,7 @@ public class ProductService {
 
 
     public void createProduct(ProductRequest productRequest){
-   Product product =      Product.builder()
+   Product product =  Product.builder()
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
@@ -24,7 +27,24 @@ public class ProductService {
         productRepo.save(product);
         log.info("Product {} is saved" , product.getId());
     }
-      // save it to the database
+
+    public List<ProductResponse> getAllProduct() {
+
+            List<Product> allproducts =      productRepo.findAll();
+            // return the list using stream   stream.toList()  returns unmodifyable list
+            // wherein strem.collect(Collectores.toList()) produces mutable arrayLIst
+          return    allproducts.stream().map(this::convertToResponse).toList();
+    }
+
+    private ProductResponse convertToResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
 
 
 }
